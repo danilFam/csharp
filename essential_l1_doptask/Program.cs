b0f80827d8dc1d8ff3fb4dc60efbf76bb8b952f6
danilFam <60857767+danilFam@users.noreply.github.com>
2020-02-18 18:59:13 +0200

    essential_l1_2 tasks

---
+++ essential_l1_doptask/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace essential_l1_doptask
+{
+    class Address
+    {
+        private string index, house, apartment;
+        private string country, city, street;
+
+        public string Index
+        {
+            set { index = value; }
+            get { return index; }
+        }
+
+        public string House
+        {
+            set { house = value; }
+            get { return house; }
+        }
+        public string Apartment
+        {
+            set { apartment = value; }
+            get { return apartment; }
+        }
+        public string Country
+        {
+            set { country = value; }
+            get { return country; }
+        }
+        public string City
+        {
+            set { city = value; }
+            get { return city; }
+        }
+        public string Street
+        {
+            set { street = value; }
+            get { return street; }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            Console.WriteLine("Введите страну:");
+            address.Country = Console.ReadLine();
+            Console.WriteLine("Введите город:");
+            address.City = Console.ReadLine();
+            Console.WriteLine("Введите улицу:");
+            address.Street = Console.ReadLine();
+            Console.WriteLine("Введите индекс:");
+            address.Index = Console.ReadLine();
+            Console.WriteLine("Введите дом:");
+            address.House = Console.ReadLine();
+            Console.WriteLine("Введите квартиру:");
+            address.Apartment = Console.ReadLine();
+            Console.WriteLine($"Вы проживаете по адресу: {address.Country}, {address.Index}, г.{address.City}, ул.{address.Street}, д.{address.House}, кв.{address.Apartment}") ;
+
+        }
+    }
+}
---
+++ essential_l1_doptask/essential_l1_task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace essential_l1_task2
+{
+    class Rectangle
+    {
+        private double side1=0, side2=0;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+       
+        public double AreaCalculator(double side1, double side2)
+        {
+           return side1 * side2;
+        
+        }
+        public double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+
+        public double Area
+        {
+            get { return AreaCalculator(side1,side2); }
+        }
+        public double Perimeter
+        {
+            get { return PerimeterCalculator(); }
+        }
+    } 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину прямоугольника:");
+            double  side1=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ширину прямоугольника:");
+            double  side2= Convert.ToDouble(Console.ReadLine());
+            Rectangle sides = new Rectangle(side1, side2);
+            Console.WriteLine("Площадь прямоугольника = {0}",sides.Area);
+            Console.WriteLine("Периметр прямоугльника = {0}", sides.Perimeter);
+
+        }
+    }
+}
